83d6bf54afcba9adb4443b0fc8d06213e5c18004
guoqing2013 <guoqing2013@163.com>
2018-03-09 21:38:28 +0800

    node

--- code/05 - http服务端/get.js
+++ code/05 - http服务端/get.js
@@ -7,4 +7,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     //解析并打成字符串形式返回给客户端
     res.end(util.inspect(urls.parse(req.url, true)));
-}).listen(3000);
+}).listen(4000);
