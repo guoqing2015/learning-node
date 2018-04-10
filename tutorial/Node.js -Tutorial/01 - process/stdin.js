8f3d3332a032a2267cd87b7583ffe798e3e398ca
guoqing2013 <guoqing2013@163.com>
2018-04-10 20:24:00 +0800

    node

--- tutorial/Node.js -Tutorial/01 - process/stdin.js
+++
@@ -1,5 +0,0 @@
-//»Ö¸´Á÷
-process.stdin.resume();
-process.stdin.on('data',function(data){
-	process.stdout.write('read from console'+data.toString());
-})
--- tutorial/Node.js -Tutorial/03 - events/app.js
+++
@@ -1,6 +0,0 @@
-var events = require('events');
-var emitter = new events.EventEmitter();
-emitter.on('someEvent', function (arg1, arg2) {
-	console.log('Listener', arg1, arg2);
-});
-emitter.emit('someEvent', 'marico', 1991)
