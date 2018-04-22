e2e88342e3c4441bbcb421d5a23f6a1d1be06644
guoqing2013 <guoqing2013@163.com>
2018-04-22 09:03:37 +0800

    express-note

--- tutorial/Node.js + Express/04 - Setup of Express/myapp/app.js
+++ tutorial/Node.js + Express/04 - Setup of Express/myapp/app.js
@@ -23,8 +23,10 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.use('/', index);
 app.use('/users', users);
--- tutorial/Node.js + Express/04 - Setup of Express/myapp/routes/index.js
+++ tutorial/Node.js + Express/04 - Setup of Express/myapp/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', condition: true, anyArray: [1, 2, 3] });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
 });
+
+router.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/')
+})
 
 module.exports = router;
